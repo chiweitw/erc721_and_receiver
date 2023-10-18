e4637dfe98f29c273ebed145211de725de8fb502
chiwei.huang <chiwei.huang@chiweihuangdeMacBook-Air.local>
2023-10-18 22:04:53 +0800

    test mint

---
+++ src/NFTReceiver.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "./NONFT.sol";
+
+contract NFTReceiver is IERC721Receiver {
+    NONFT private nft;
+    address private owner;
+
+    constructor(address _nftAddress) {
+        nft = NONFT(_nftAddress);
+        owner = msg.sender;
+    }
+
+    function onERC721Received(
+        address operator,
+        address from,
+        uint256 tokenId,
+        bytes memory data
+    ) external override returns (bytes4) {
+        // check if the sender is the ERC721 contract
+        require(msg.sender == address(nft));
+
+        return IERC721Receiver.onERC721Received.selector;
+    }
+}
--- test/NONFT.t.sol
+++ test/NONFT.t.sol
@@ -3,20 +3,21 @@ pragma solidity ^0.8.20;
 
 import {Test, console2} from "forge-std/Test.sol";
 import {NONFT} from "../src/NONFT.sol";
+import {NFTReceiver} from "../src/NFTReceiver.sol";
 
 contract NONFTTest is Test {
     NONFT public nft;
-    address public receiver;
+    NFTReceiver public receiver;
    
     function setUp() public {
-        receiver = makeAddr("Receiver");
+        nft = new NONFT();
+        receiver = new NFTReceiver(address(nft));
     }
 
+    // it should mint a new token
     function testMint() public {
         uint256 tokenId = 1;
-        // it should mint a new token
-        nft.mint(receiver, tokenId);
-
-        assertEq(nft.ownerOf(tokenId), receiver);
+        nft.mint(address(receiver), tokenId);
+        assertEq(nft.ownerOf(tokenId), address(receiver));
     }
 }
